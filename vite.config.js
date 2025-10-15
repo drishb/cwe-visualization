0ca3c83998184fd691815b809eac0d3ca254d364
l <67603818+drishb@users.noreply.github.com>
2025-10-15 15:03:44 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import { version } from 'node:os'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/cwe-navigation/',
+  base: '/cwe-visualization/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
